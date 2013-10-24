740a7c4869e8fcbd7b5945a0fab4b4940bf26aa0
Adam Witko <adam.witko@gmail.com>
2013-10-24 19:13:06 +0100

    Adding loads of stuff

---
+++ TeamSauce.Test/Class1.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace TeamSauce.Test
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void PassPlease()
+        {
+            Assert.That(true, Is.True);
+        }
+    }
+}
